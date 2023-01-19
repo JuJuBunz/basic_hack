2ba82f987868b725224f65d07918272ac8d98e31
JuJuBunz <123039916+JuJuBunz@users.noreply.github.com>
2023-01-19 13:41:29 -0800

    Add files via upload

---
+++ basic_hack.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# this script is made by Julian B Guthub: JuJuBunz
+echo "booting... please stand by"
+sleep 3
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNXO0XNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNX0OXNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNOookXNNNNNNNNNNNNNNNNNNNNNNNNNXK00KXNNNNNNNNNNNNNNNNNNNNNNNXkooONNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNXOo;;oKNNNNNNNNNNNNNNNNNNNNKxc,'...,lONNNNNNNNNNNNNNNNNNNKo;;oOXNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNxcol;;lx0NNNNNNNNNNNNNNNNKo,.;clll:'..oKNNNNNNNNNNNNNNN0xo;;locxNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNO'.kNklc:lxKNNNNNNNNNNNNXx;,oOOdoodk0x;.:KNNNNNNNNNNNNKxl:clkNk.'ONNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNo..c0XOc;,':odOXNNNNNNN0c'l0k:....'';x0d';ONNNNNNNXOdo:',;cOX0c..oNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNd.,;;;;;;:clc'.dNNNNNXx,;k0lcc.'oOd' .;OO;;ONNNNNNd.'clc:;;;;;;,.oNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNK:.:lllloodxx,.o0XNNOc'o0x,.'dOKOd; ....dO:,xXNNX0o.,xxdolllll:.:0NNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNKd:,',,;;,..,okxOOc';kOc....l0kkOko;....cOl'lX0llko;..,;;,,',:dKNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNXOxollloxOkodo;.,dOo'....cx:..';okko,..;kd,:dxd;'okdollloxOXNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNXOxoll:;...;oxo'......'........;odo:''dkc,od:l0NNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNXd,.. ..,:clol:..  .....'',,,,,,'....,,..;xxc:d0NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNN0; ....,;;,'. .';:ccccoOK0OkddxO00kdol:,...;ddlclxOKKKKKXNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNk;.........';;::;,;llll:,.....,coddoolllc:;'':c:::,'''',cxKNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNKxl,......'.. .,cc:;:lxd:':ddollc:;.. ..',,'........... .lXNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNXOo,.......;:',lOXNNNXkc;oXNNNX0xl,.... ..........:;..cXNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNXkl,.....'o0XNNNNNNNKxll0XXNNNNXd'......... .,ll'.c0NNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNKkl'.:0XkooxKNNXOOX0:';;coxxoc:........';::,'cOXNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNKocxd'...,lO0o:c;..........'.......',,,:d0NNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNXlld.  .cx0NX0Ox;...... ..... ..';cokKNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNXldXxccxXNNKl,'lOdc:;;:lkl.'coxO0XNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNX0xldXXOddkKo....oKkookXXdcokXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNXOc'lkoc;..:KKl:dd:lKK:..;cokl':OXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNXx:';:;'lk,.:o0NNNNNNNN0o:.,kl';:;':xKNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNXOo;;cc;...lO:.,;cdxxxxxxoc;,.:Ol...;cc;;oOXNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNKd:,col;..'ckXO;od,;:cc::cc:;;do;OXkc'..;loc,:dKNNNNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNXkc;:loc'..:d0NNNXxllc:,,;;;;,,:cllxXNNNKd:..'col:;ckXNNNNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNOo;;col;..,oOXNNNNNNNXkcokkkOOkkkocxXNNNNNNNXOo,..;loc;;oOXNNNNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNN0dc;:cc;..,lkXNNNNNNNNNNNN0llddddddll0NNNNNNNNNNNNKkl,..;cc:;cd0NNNNNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNXOd:'.',,',cdOXNNNNNNNNNNNNNNNNN0kkxkkk0NNNNNNNNNNNNNNNNNXOdc,',,'.':dOXNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNKxc,,;:cox0XNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNX0xoc:;,,:xXNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNXXXXXNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNXXXXXNNNNNNNNNNNNNN"
+echo "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+sleep 3
+echo "welcome hacker..."
+echo "Please use this tool for EdUcAtIoNaL PuRpOsEsS OnLy!"
+echo "Github account: https://github.com/JuJuBunz"
+PS3='Please select your option: '
+options=("Phishing" "Wifi Limiter" "reverse shell" "wifi passwd cracker" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Phishing")
+            echo "Downloading and starting a phishing page crator tool"
+            sleep 3
+            git clone --depth=1 https://github.com/htr-tech/zphisher.git
+            cd zphisher
+            bash zphisher.sh
+            ;;
+        "Wifi Limiter")
+            echo "limit peoples wifi with this tool, for more info do -h in in evillimiter" 
+            sleep 3
+            git clone https://github.com/bitbrute/evillimiter.git
+            cd evillimiter
+            python3 setup.py install
+            sudo evillimiter 
+            ;;
+        "reverse shell")
+            echo "this tool makes a mshta cmd to type in the powershell of the victim, only works if you have realtime protection off. Type run once inside koadic"
+            sleep 5
+            sudo apt-get install koadic
+            koadic
+            ;;
+        "wifi passwd cracker")
+            sleep 4
+            echo "installing wifite if you dont already have it..."
+            sudo apt-get install wifite
+            echo "you need a wlan0 wifi adpater for this... booting wifite"
+            sleep 3
+            sudo wifite
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "Invalid option bro $REPLY";;
+    esac
+done
+
+#thanks chat gpt for helping with some confusing parts of the code! 
